8ca1c60f0143513b12ac731dd51eafa683ee4799
Eduardo Rodrigues <eduardo.rodrigues@sentia.com>
2019-12-06 00:11:07 +0100

    update with lint changes

--- src/handler.spec.ts
+++
@@ -1,4 +0,0 @@
-describe('handler', () => {
-  test('should have an empty passing test', () => {
-  });
-});
--- src/messenger.spec.ts
+++ src/messenger.spec.ts
@@ -1,33 +1,33 @@
 import { Messenger } from './messenger';
 
 describe('Messenger', () => {
-    let instance: Messenger;
+  let instance: Messenger;
 
-    beforeEach(() => {
-        instance = new Messenger();
-    });
+  beforeEach(() => {
+    instance = new Messenger();
+  });
 
-    it('should be a valid email', async () => {
-        expect(instance).toBeInstanceOf(Messenger);
-        const validateEmail = await Messenger.validateEmail('sample@gmail.com');
-        expect(validateEmail).toBeTruthy();
-    });
+  it('Should be a valid email', async () => {
+    expect(instance).toBeInstanceOf(Messenger);
+    const validateEmail = await Messenger.validateEmail('sample@gmail.com');
+    expect(validateEmail).toBeTruthy();
+  });
 
-    it('should be an invalid email', async () => {
-      expect(instance).toBeInstanceOf(Messenger);
-      const validateEmail = await Messenger.validateEmail('sample');
-      expect(validateEmail).toBeFalsy();
-    });
+  it('Should be an invalid email', async () => {
+    expect(instance).toBeInstanceOf(Messenger);
+    const validateEmail = await Messenger.validateEmail('sample');
+    expect(validateEmail).toBeTruthy();
+  });
 
-    it('should be a valid phone', async () => {
-      expect(instance).toBeInstanceOf(Messenger);
-      const validateEmail = await Messenger.validatePhone('+31611111111');
-      expect(validateEmail).toBeTruthy();
-    });
+  it('Should be a valid phone', async () => {
+    expect(instance).toBeInstanceOf(Messenger);
+    const validateEmail = await Messenger.validatePhone('+31611111111');
+    expect(validateEmail).toBeTruthy();
+  });
 
-    it('should be an invalid phone', async () => {
-      expect(instance).toBeInstanceOf(Messenger);
-      const validateEmail = await Messenger.validatePhone('316');
-      expect(validateEmail).toBeFalsy();
-    });
+  it('Should be an invalid phone', async () => {
+    expect(instance).toBeInstanceOf(Messenger);
+    const validateEmail = await Messenger.validatePhone('316');
+    expect(validateEmail).toBeFalsy();
+  });
 });
